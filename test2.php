f5ae2b682ef231c9069151a6aff8350700d739d8
Your NameKumar <brilliant88work@gmail.com>
2017-05-17 10:49:25 +0530

    test

--- test2.php
+++ test2.php
@@ -19,12 +19,3 @@ echo 'another change 123146'; die;
 echo 'another change 12314678910111'; die;
 ?>
 
-<<<<<<< HEAD
-
-<?php 
-echo 'another change testy'; die;
-=======
-<?php 
-echo 'another change common man'; die;
->>>>>>> 81fb4c06db049949318486ab8221fc758794e922
-?>
